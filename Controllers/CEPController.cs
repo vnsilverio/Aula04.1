47790947b18b7276a808f61c263b3a6d803891db
Aluno - Lab <Aluno - Lab@MMD-LAB02-62>
2026-03-10 21:35:03 -0300

    Adição cep

---
+++ Controllers/CEPController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula04._1.Controllers
+{
+    public class CEPController : Controller
+    {
+        public IActionResult CEP()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/CEP/CEP.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aula04._1.Views.CEP
+{
+    public class CEPModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
